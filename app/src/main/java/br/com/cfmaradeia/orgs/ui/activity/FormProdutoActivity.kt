75b5fe7d508c03bdaac4cc7d40b1cb83dc831f08
Carlos Felipe Maradeia <carlos.maradeia@loft.com.br>
2025-02-23 00:54:12 -0300

    Add product form

---
+++ app/src/main/java/br/com/cfmaradeia/orgs/ui/activity/FormProdutoActivity.kt
@@ -0,0 +1,12 @@
+package br.com.cfmaradeia.orgs.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FormProdutoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
